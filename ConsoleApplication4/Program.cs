5bb09c33a8e9149877833ada136f0df1ce696df0
Juyoung Choi <juy@outlook.dk>
2017-02-01 14:21:35 +0100

    initial commit

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4 {
+    class Program {
+        static void Main(string[] args) {
+            QueryStringBuilder qsb = new QueryStringBuilder("http//fkj.dk");
+            qsb.Add("x", 5);
+            qsb.Add("y", "23 23");
+            qsb.Add("z", 9);
+
+
+            Console.WriteLine((qsb.ToString())); 
+        }
+    }
+}
---
+++ ConsoleApplication4/QueryStringBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class QueryStringBuilder {
+    private string url;
+    private Dictionary<string, object> qsb = new Dictionary<string, object>();
+    private string text;
+    public QueryStringBuilder(string url) {
+        this.url = url;
+    }
+
+    public void Add(string identifier, object value) {
+        qsb.Add(identifier, value);
+
+    }
+
+    public string BuildQuery() {
+        StringBuilder sb = new StringBuilder();
+        sb.Append(url);
+
+        int queryLength = qsb.Count();
+        if (queryLength != 0) {
+            sb.Append("?");
+            if (queryLength == 1) {
+                sb.Append(qsb.ElementAt(0).Key + "=" + WebUtility.UrlEncode(qsb.ElementAt(0).Value.ToString()));
+
+            }
+            else {
+                for (int i = 0; i < queryLength-1; i++) {
+                    sb.Append(qsb.ElementAt(i).Key + "=" + WebUtility.UrlEncode(qsb.ElementAt(i).Value.ToString()));
+                    sb.Append("&");
+                }
+                sb.Append(qsb.ElementAt(queryLength-1).Key + "=" + WebUtility.UrlEncode(qsb.ElementAt(queryLength-1).Value.ToString()));
+
+            }
+        }
+
+        return sb.ToString();
+    }
+
+   
+    public override string ToString() {
+        return BuildQuery();
+    }
+
+}
